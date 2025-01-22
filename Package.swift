77a34d1dc3c5b15023c09e853553f2332800481d
spvagelis <spirou.vagelis@gmail.com>
2025-01-22 13:31:31 +0200

    - Remove CocoaLumberjack dependency - Added sharedSPM dependency

--- Package.swift
+++ Package.swift
@@ -13,14 +13,15 @@ let package = Package(
             type: .dynamic,
             targets: ["AntifraudSPM"]),
     ],
-    dependencies: [.package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", from: "3.8.0"),
-                  ],
+    dependencies: [
+        .package(url: "https://github.com/spvagelis/sharedSPM.git", exact: "1.0.2")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "AntifraudSPM",
-            dependencies: [.target(name: "AntifraudSDK"), .product(name: "CocoaLumberjack", package: "CocoaLumberjack")]
+            dependencies: [.target(name: "AntifraudSDK"), .product(name: "sharedSPM", package: "sharedSPM")]
         ),
         .testTarget(
             name: "AntifraudSPMTests",
